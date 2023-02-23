bd24c273dc3ca75f686c70a8f3c9dbfaced7a39d
Donald Nwokoro <infect3dlab@gmail.com>
2023-02-23 21:20:28 +0100

    Android Currency Converter

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
